4a4640b03a6371f06fd2c6c5f8b1e089ae3909ed
Shaurya Mohan <shaurya.mhn@gmail.com>
2020-07-09 14:14:45 +0530

    Updates

---
+++ precache-manifest.c14b751d298572ba5870ab7400377180.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf175b5b9e2d26a10c60ba4a90cda594",
+    "url": "/TLF/index.html"
+  },
+  {
+    "revision": "d45f9fabaa25e4316a9c",
+    "url": "/TLF/static/css/main.d51c245a.chunk.css"
+  },
+  {
+    "revision": "b2aa13a575ce5ba9cad4",
+    "url": "/TLF/static/js/2.df8ee2c5.chunk.js"
+  },
+  {
+    "revision": "ccc502121842bd2ec2d999aa7d823359",
+    "url": "/TLF/static/js/2.df8ee2c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d45f9fabaa25e4316a9c",
+    "url": "/TLF/static/js/main.744c490e.chunk.js"
+  },
+  {
+    "revision": "cf77857a8da4460579cc",
+    "url": "/TLF/static/js/runtime-main.239a1842.js"
+  },
+  {
+    "revision": "75eae2f5dd4651739fedc5aad4639cca",
+    "url": "/TLF/static/media/Covid.75eae2f5.jpg"
+  },
+  {
+    "revision": "60cc2394d005259a2cb60af6ec3a91f6",
+    "url": "/TLF/static/media/Racism.60cc2394.jpg"
+  },
+  {
+    "revision": "4b837e5e1fdffe0208b557eddcc06223",
+    "url": "/TLF/static/media/four.4b837e5e.jpg"
+  },
+  {
+    "revision": "8088146042efaaaf61aa7136df659843",
+    "url": "/TLF/static/media/logo.80881460.png"
+  },
+  {
+    "revision": "5cf06ed5fb100391c2124e32d0545a15",
+    "url": "/TLF/static/media/one.5cf06ed5.jpg"
+  },
+  {
+    "revision": "a57a3fe32ebc490b4d64c69fd72016cd",
+    "url": "/TLF/static/media/three.a57a3fe3.jpg"
+  },
+  {
+    "revision": "00dc9e29ca74cf96ec151460f26ace0b",
+    "url": "/TLF/static/media/topbackground.00dc9e29.jpg"
+  },
+  {
+    "revision": "984d3367e090ecf1b9c096a8223bcdf8",
+    "url": "/TLF/static/media/two.984d3367.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-gh-pages/precache-manifest.5697dea03a7d24ad8c2045152969a99f.js"
+  "/TLF/precache-manifest.c14b751d298572ba5870ab7400377180.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-gh-pages/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/TLF/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });

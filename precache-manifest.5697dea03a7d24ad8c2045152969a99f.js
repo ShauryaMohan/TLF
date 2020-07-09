99af17edd981314c60b728fdb7aa96cc7565ecc4
Shaurya Mohan <shaurya.mhn@gmail.com>
2020-07-09 14:09:22 +0530

    Updates

---
+++ precache-manifest.5697dea03a7d24ad8c2045152969a99f.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22e87eb60785f433bdbf814287b3927e",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "dd960a9563028b862394",
+    "url": "/react-gh-pages/static/css/main.127c5677.chunk.css"
+  },
+  {
+    "revision": "814c1192532140c5017e",
+    "url": "/react-gh-pages/static/js/2.9d66ccf0.chunk.js"
+  },
+  {
+    "revision": "ccc502121842bd2ec2d999aa7d823359",
+    "url": "/react-gh-pages/static/js/2.9d66ccf0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd960a9563028b862394",
+    "url": "/react-gh-pages/static/js/main.90add4bf.chunk.js"
+  },
+  {
+    "revision": "71db1f69ad72095b4d2f",
+    "url": "/react-gh-pages/static/js/runtime-main.997c5c6c.js"
+  },
+  {
+    "revision": "75eae2f5dd4651739fedc5aad4639cca",
+    "url": "/react-gh-pages/static/media/Covid.75eae2f5.jpg"
+  },
+  {
+    "revision": "60cc2394d005259a2cb60af6ec3a91f6",
+    "url": "/react-gh-pages/static/media/Racism.60cc2394.jpg"
+  },
+  {
+    "revision": "4b837e5e1fdffe0208b557eddcc06223",
+    "url": "/react-gh-pages/static/media/four.4b837e5e.jpg"
+  },
+  {
+    "revision": "8088146042efaaaf61aa7136df659843",
+    "url": "/react-gh-pages/static/media/logo.80881460.png"
+  },
+  {
+    "revision": "5cf06ed5fb100391c2124e32d0545a15",
+    "url": "/react-gh-pages/static/media/one.5cf06ed5.jpg"
+  },
+  {
+    "revision": "a57a3fe32ebc490b4d64c69fd72016cd",
+    "url": "/react-gh-pages/static/media/three.a57a3fe3.jpg"
+  },
+  {
+    "revision": "00dc9e29ca74cf96ec151460f26ace0b",
+    "url": "/react-gh-pages/static/media/topbackground.00dc9e29.jpg"
+  },
+  {
+    "revision": "984d3367e090ecf1b9c096a8223bcdf8",
+    "url": "/react-gh-pages/static/media/two.984d3367.jpg"
+  }
+]);
